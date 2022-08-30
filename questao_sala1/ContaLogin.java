f218426c3b801cc955b0470c1130613591e03a54
luanastx <102376587+luanastx@users.noreply.github.com>
2022-08-30 02:59:53 -0300

    Add files via upload

---
+++ questao_sala1/ContaLogin.java
@@ -0,0 +1,22 @@
+public class ContaLogin {
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    String nome;
+    String senha;
+}
+
