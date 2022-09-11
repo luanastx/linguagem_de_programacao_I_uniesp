9a256e8339f5ea22a59be8f4e597de63d0c3b064
luanastx <102376587+luanastx@users.noreply.github.com>
2022-09-11 20:59:00 -0300

    Add files via upload

---
+++ ted05.java
@@ -0,0 +1,21 @@
+
+public class ted05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 int i = 1;
+	        while (i <= 20) {
+	            System.out.print(i);
+	            if (i % 5 == 0){
+	                System.out.println();
+	            }
+	            else {
+	                System.out.print('\t');
+	            }
+	            ++i;
+	        }
+	    
+	}
+
+}
